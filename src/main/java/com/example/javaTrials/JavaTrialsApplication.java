49e1012cd5999c1567218787df69145f3b01f87b
rinithprasad <rinithprasad@tamu.edu>
2019-05-09 21:44:39 -0500

    commit

---
+++ src/main/java/com/example/javaTrials/JavaTrialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaTrials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaTrials/JavaTrialsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.javaTrials;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaTrialsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
